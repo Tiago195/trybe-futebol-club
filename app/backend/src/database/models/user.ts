97b4cde4911f5245cc45546afcb501efbb460c18
Tiago Meireles <tiagocf475@gmail.com>
2022-07-06 18:12:44 -0300

    ta tudo com saude

--- app/backend/src/database/models/user.ts
+++ app/backend/src/database/models/user.ts
@@ -1,1 +1,32 @@
-import { Model } from 'sequelize';
+import { Model, DataTypes } from 'sequelize';
+import db from "./"
+
+class User extends Model {}
+
+User.init({
+  id: {
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false
+  },
+  username: {
+    type: DataTypes.STRING,
+  },
+  role: {
+    type: DataTypes.STRING,
+  },
+  email: {
+    type: DataTypes.STRING,
+  },
+  password: {
+    type: DataTypes.STRING,
+  }
+}, {
+  timestamps: false,
+  underscored: true,
+  sequelize: db,
+  modelName: "users"
+})
+
+export default User
