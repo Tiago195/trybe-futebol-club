984bf987b450185e44c5e0d74ce8061d0a96df5a
Tiago195 <tiagocf475@gmail.com>
2022-07-07 17:03:42 -0300

    :sparkles: create user controller and implement login function

---
+++ app/backend/src/controllers/user.controller.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from '../utils/jwt';
+import UserService from '../services/user.service';
+
+class UserController {
+  public service: UserService;
+  constructor(service: UserService) {
+    this.service = service;
+  }
+
+  async getFromEmailAndPassword(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { email, password } = req.body;
+      const user = await this.service.getFromEmailAndPassword(email, password);
+
+      const token = jwt.encode(user);
+
+      res.status(200).json({ token });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export default UserController;
