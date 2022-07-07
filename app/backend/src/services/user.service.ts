ca156263df8fc1bb9f2604eab6e8aead0ea9a79d
Tiago195 <tiagocf475@gmail.com>
2022-07-07 18:11:18 -0300

    :lock: fix: add new verifications in userService
    verificando se o password enviado pelo user é igual ao passoword criptografado do bd da func getFromAndPassword

--- app/backend/src/services/user.service.ts
+++ app/backend/src/services/user.service.ts
@@ -1,8 +1,17 @@
-import User from '../database/models/user';
+import * as bc from 'bcryptjs';
+import generateErrorObj from '../utils/generateErrorObj';
+import User, { UserAttributes } from '../database/models/user';
 
 class UserService {
   getFromEmailAndPassword = async (email: string, password: string) => {
-    const user = await User.findOne({ where: { email, password } });
+    const response = await User.findOne({ where: { email } });
+
+    const { password: passwordRemoved, ...user } = response?.get() as UserAttributes;
+
+    const isPassword = await bc.compare(password, passwordRemoved);
+
+    if (!isPassword) throw generateErrorObj('Incorrect email or password', 401);
+
     return user;
   };
 }
