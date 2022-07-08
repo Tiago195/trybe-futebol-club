e8701de4b46a4ae37145ecb7df4280ffe55fcb95
Tiago Meireles <tiagocf475@gmail.com>
2022-07-08 16:11:00 -0300

    arrumando lint

--- app/backend/src/services/user.service.ts
+++ app/backend/src/services/user.service.ts
@@ -6,7 +6,7 @@ class UserService {
   getFromEmailAndPassword = async (email: string, password: string) => {
     const response = await User.findOne({ where: { email } });
 
-    if(!response) throw generateErrorObj('Incorrect email or password', 401);
+    if (!response) throw generateErrorObj('Incorrect email or password', 401);
 
     const { password: passwordRemoved, ...user } = response?.get() as UserAttributes;
 
