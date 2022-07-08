73e5a272a959f00f9dbe4f90db69a101de4d8c8c
Tiago Meireles <tiagocf475@gmail.com>
2022-07-08 16:36:48 -0300

    importando teamRouter no app

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import genericError from './middlewares/genericError';
 import userRouter from './routes/user.routes';
+import teamRouter from './routes/team.routes';
 
 class App {
   public app: express.Express;
@@ -25,6 +26,7 @@ class App {
     this.app.use(express.json());
     this.app.use(accessControl);
     this.app.use(userRouter);
+    this.app.use(teamRouter);
     this.app.use(genericError);
   }
 
