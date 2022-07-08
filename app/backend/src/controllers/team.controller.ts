1132c5c9d90ea034f1a0de4f3da0919743f37673
Tiago Meireles <tiagocf475@gmail.com>
2022-07-08 17:00:22 -0300

    add func getById in teams controller

--- app/backend/src/controllers/team.controller.ts
+++ app/backend/src/controllers/team.controller.ts
@@ -7,6 +7,7 @@ class TeamController {
     this.service = service;
 
     this.getAll = this.getAll.bind(this);
+    this.getById = this.getById.bind(this);
   }
 
   async getAll(req: Request, res: Response, next: NextFunction) {
@@ -14,6 +15,18 @@ class TeamController {
       const teams = await this.service.getAll();
 
       res.status(200).json(teams);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+
+      const team = await this.service.getById(Number(id));
+
+      res.status(200).json(team);
     } catch (error) {
       next(error);
     }
